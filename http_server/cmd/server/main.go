42dd8e752771fc3014509910af3d60599b262128
kraitsura <b.aarya.reddy@gmail.com>
2025-10-25 04:33:46 -0700

    routes and status codes

--- http_server/cmd/server/main.go
+++ http_server/cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -26,9 +27,47 @@ func main() {
 			continue
 		}
 
-		fmt.Println("Connected Accepted from:", conn.RemoteAddr())
-
-		conn.Close()
-
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+
+	// Lets create buffer and read the curl req
+	buffer := make([]byte, 1024)
+
+	n, err := conn.Read(buffer)
+
+	if err != nil {
+		log.Println("Error while reading buffer:", err)
+		conn.Close()
+	}
+
+	fmt.Println(string(buffer[:n]))
+
+	lines := strings.Split(string(buffer[:n]), "\r\n")
+
+	firstLine := lines[0]
+
+	route := strings.Split(firstLine, " ")[1]
+	var responseBody string
+	var statusLine string
+	switch route {
+	case "/":
+		statusLine = "HTTP/1.1 200 OK"
+		responseBody = "Home Route"
+	case "/hello":
+		statusLine = "HTTP/1.1 200 OK"
+		responseBody = "Hello Route"
+	default:
+		statusLine = "HTTP/1.1 404 Not Found"
+		responseBody = "404! Route not found"
+	}
+
+	fmt.Println("Connected Accepted from:", conn.RemoteAddr())
+	contentLength := len(responseBody)
+	response := fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", statusLine, contentLength, responseBody)
+	conn.Write([]byte(response))
+
+	conn.Close()
 }
